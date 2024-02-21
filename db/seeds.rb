e3f2c9fbbbb380b25b6a84bd1769955f1c7b361d
Tharun98 <tharundtr@tamu.edu>
2024-02-21 10:29:35 -0600

    Fix seeds issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,11 +18,11 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
   {:title => 'Mission Impossible 7', :rating => 'PG-13',
-    :release_date => '13-July-2023'}
+    :release_date => '13-July-2023'},
   {:title => 'Oppenheimer', :rating => 'R',
-    :release_date => '21-July-2023'}
+    :release_date => '21-July-2023'},
   {:title => 'John Wick 4', :rating => 'R',
     :release_date => '24-Mar-2023'}
 
